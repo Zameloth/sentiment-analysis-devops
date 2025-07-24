824a7a18f14b6a9b46ff376dce68012fa7640a59
Théo ELOY <theoeloy12@gmail.com>
2025-07-24 21:17:13 +0200

    Init project

---
+++ main.py
@@ -0,0 +1,35 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+from transformers import (
+    AutoTokenizer,
+    TFAutoModelForSequenceClassification,
+    pipeline
+)
+
+app = FastAPI()
+
+tokenizer = AutoTokenizer.from_pretrained("alosof/camembert-sentiment-allocine", use_fast=False)
+model     = TFAutoModelForSequenceClassification.from_pretrained("alosof/camembert-sentiment-allocine")
+
+classifier = pipeline(
+    "text-classification",
+    model=      model,
+    tokenizer=  tokenizer,
+    framework=  "tf"
+)
+
+
+class InputText(BaseModel):
+    text: str
+
+@app.get("/health")
+def health():
+    return {"status": "ok"}
+
+@app.post("/predict")
+def predict(input_data: InputText):
+    result = classifier(input_data.text)[0]
+    return {
+        "label":  result["label"],
+        "score":  round(result["score"], 3)
+    }

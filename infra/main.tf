9da6e38654e9d44ba12215c1789592bf1c17cce2
Théo ELOY <theoeloy12@gmail.com>
2025-07-25 15:24:57 +0200

    Update Terraform configuration: Modify app service plan `sku_name` from `BI` to `B1`

--- infra/main.tf
+++ infra/main.tf
@@ -44,7 +44,7 @@ resource "azurerm_service_plan" "model_plan" {
   name                = "${var.app_service_plan_name}-model"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
-  sku_name            = "BI"  # Plus puissant pour le modèle
+  sku_name            = "B1"
   os_type             = "Linux"
 }
 
